17e04c7d4b4fc5e2b78e7252b7d36a630508fab4
Ragim <147828799+gadzhimuslimov@users.noreply.github.com>
2024-02-04 20:57:25 +0300

    task.py

--- ООП Лабораторная №3/task.py
+++ ООП Лабораторная №3/task.py
@@ -1,8 +1,8 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -10,22 +10,53 @@ class Book:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
+    @property
+    def name(self):
+        return self._name
 
-class PaperBook:
+    @property
+    def autor(self):
+        return self._author
+
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super.__init__()
+        self._pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages) -> None:
+        """Устанавливает количество страниц в книге."""
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
+
+    def __repr__(self):
+        super.__repr__()
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__()
+        self._duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def pages(self, new_duration) -> None:
+        """Устанавливает количество страниц в книге."""
+        if not isinstance(new_duration, float):
+            raise TypeError("Продолжительность аудиокниги должно быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Продолжительность аудиокниги должно быть положительным числом")
+        self._duration = new_duration
+
+    def __repr__(self):
+        super.__repr__()
